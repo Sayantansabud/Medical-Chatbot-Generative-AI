03c70bfcb753a156f78e415a56e74feedf2a0746
Sayantan Sabud <sayantansabud5254@gmail.com>
2025-06-05 09:25:34 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Project',
+    version='0.0.0',   
+    author= 'Sayantan Sabud',
+    author_email= 'sayantansabud5254@gmail.com',
+    packages=find_packages(),
+    install_requires=[ ],
+
+)
